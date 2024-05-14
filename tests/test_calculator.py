e7830271e65273b9a2bca8153544368018881248
ksj <ksj@hims.co.kr>
2024-05-14 17:14:39 +0900

    add subtract test code

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,3 +6,10 @@ import pytest
 ])
 def test_add(cal, num1, num2, expected_result):
     assert expected_result == cal.add(num1, num2)
+    
+@pytest.mark.parametrize("num1, num2, expected_result",[
+    (1, 2, -1),
+    (2, 3, -1)
+])
+def test_subtract(cal, num1, num2, expected_result):
+    assert expected_result == cal.subtract(num1, num2)
